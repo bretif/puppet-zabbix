8db517331776ecee87de65d29b78327e28a228ab
Bertrand Rétif <br@phosphore.eu>
2014-10-16 13:44:26 +0200

    Add zabbix-nginx_check module

---
+++ manifests/scripts/nginx_check.pp
@@ -0,0 +1,49 @@
+# == Class: zabbix::scripts::backuppc
+#
+#  This will install backuppc script used to monitor nginx
+#
+# === Authors
+#
+# Author Name: bretif@phosphore.eu
+#
+# === Copyright
+#
+# Copyright 2014 Bertrand RETIF
+#
+class zabbix::scripts::nginx_check () {
+  
+  file { "/etc/zabbix/scripts":
+                owner   => "zabbix",
+                group   => "zabbix",
+                mode    => 0755,
+                ensure  => [directory, present],
+        }
+
+  file { '/etc/zabbix/scripts/nginx-check.sh':
+    ensure  => present,
+    owner   => 'zabbix',
+    group   => 'zabbix',
+    mode    => '0750',
+    replace => true,
+    source  => "puppet:///modules/zabbix/nginx-check.sh",
+    
+  }
+  
+  # Nginx vhost to activate nginx status page
+  file { '/etc/nginx/sites-available/nginx_status.conf':
+    ensure  => present,
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0644',
+    notify  => Service['nginx'],
+    require => Package['nginx'],
+    replace => true,
+    content => template('zabbix/nginx_status.conf.erb'),
+  }
+ 
+  file { '/etc/nginx/sites-enabled/nginx_status.conf':
+   ensure => 'link',
+   target => '/etc/nginx/sites-available/nginx_status.conf',
+  }
+  
+}
